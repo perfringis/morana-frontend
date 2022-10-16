2ff4454ef13d7faaf9341dcec1f4bdcc47c13475
dzidzi <kopko.mateusz@gmail.com>
2022-10-16 13:37:37 +0200

    finish first version of side bar

--- src/sites/New.jsx
+++ src/sites/New.jsx
@@ -20,78 +20,78 @@ import {
 const New = () => {
   return (
     <div>
-      <div className="h-screen w-80 bg-charleston_green">
-        <div className="pt-14">
-          <div className="mx-6 flex flex-row items-center justify-center px-6 py-3 transition hover:bg-onyx">
-            <div className="mr-6 h-6 w-6 bg-fiery_rose-0" />
-            <div className="text-xl text-white_custom">SMITE</div>
-            <div className="ml-auto">
-              <CodeBracketSquareIcon className="h-6 w-6 cursor-pointer text-roman_silver transition hover:text-white_custom" />
-            </div>
+      <div className="flex h-screen w-80 flex-col bg-charleston_green">
+        <div className="mx-6 mt-12 flex h-12 flex-row items-center justify-center px-6 transition hover:bg-onyx">
+          <div className="mr-6 h-6 w-6 bg-fiery_rose-0" />
+          <div className="text-xl text-white_custom">SMITE</div>
+          <div className="ml-auto">
+            <CodeBracketSquareIcon className="h-6 w-6 cursor-pointer text-roman_silver transition hover:text-white_custom" />
           </div>
-          <div className="mx-6 mt-12 flex h-12 flex-row items-center justify-center bg-onyx">
-            <div className="mx-6 h-6 w-6 bg-turquoise-0" />
-            <div className="text-base text-white_custom">SMITE</div>
-            <div className="ml-auto pr-6">
-              <ChevronDownIcon className="h-6 w-6 cursor-pointer text-roman_silver transition hover:text-white_custom" />
-            </div>
+        </div>
+        <div className="mx-6 mt-12 flex h-12 flex-row items-center justify-center bg-onyx">
+          <div className="mx-6 h-6 w-6 bg-turquoise-0" />
+          <div className="text-base text-white_custom">SMITE</div>
+          <div className="ml-auto pr-6">
+            <ChevronDownIcon className="h-6 w-6 cursor-pointer text-roman_silver transition hover:text-white_custom" />
           </div>
-          <div className="flex flex-col justify-center gap-y-4 pt-12">
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <TvIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Desktop App</div>
+        </div>
+        <div className="mt-12 flex flex-col justify-center gap-y-4">
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <TvIcon className="h-6 w-6" />
             </div>
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <HeartIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Support Us</div>
-            </div>
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <VideoCameraIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Live Game</div>
-            </div>
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <DocumentDuplicateIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Multi Search</div>
-            </div>
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <ListBulletIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Tier List</div>
-            </div>
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <ChartBarIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Leader Boards</div>
-            </div>
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <TrophyIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Champions</div>
-            </div>
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <WrenchScrewdriverIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Pro Builds</div>
-            </div>
-            <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
-              <div className="pr-6 pl-6">
-                <Cog6ToothIcon className="h-6 w-6" />
-              </div>
-              <div className="text-base">Settings</div>
-            </div>
+            <div className="text-base">Desktop App</div>
           </div>
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <HeartIcon className="h-6 w-6" />
+            </div>
+            <div className="text-base">Support Us</div>
+          </div>
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <VideoCameraIcon className="h-6 w-6" />
+            </div>
+            <div className="text-base">Live Game</div>
+          </div>
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <DocumentDuplicateIcon className="h-6 w-6" />
+            </div>
+            <div className="text-base">Multi Search</div>
+          </div>
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <ListBulletIcon className="h-6 w-6" />
+            </div>
+            <div className="text-base">Tier List</div>
+          </div>
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <ChartBarIcon className="h-6 w-6" />
+            </div>
+            <div className="text-base">Leader Boards</div>
+          </div>
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <TrophyIcon className="h-6 w-6" />
+            </div>
+            <div className="text-base">Champions</div>
+          </div>
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <WrenchScrewdriverIcon className="h-6 w-6" />
+            </div>
+            <div className="text-base">Pro Builds</div>
+          </div>
+          <div className="mx-6 flex cursor-pointer flex-row items-center justify-start text-roman_silver transition hover:text-white_custom">
+            <div className="pr-6 pl-6">
+              <Cog6ToothIcon className="h-6 w-6" />
+            </div>
+            <div className="text-base">Settings</div>
+          </div>
+        </div>
+        <div className="mt-auto flex flex-col gap-6">
           <div className="mx-6 flex h-12 cursor-pointer flex-row items-center justify-start bg-onyx_yellow text-base text-banana_mania">
             <div className="px-6">
               <BeakerIcon className="h-6 w-6" />
@@ -99,12 +99,12 @@ const New = () => {
             <div className="">Get God Pro</div>
           </div>
           <div className="flex h-20 flex-row items-center justify-start gap-x-6 bg-onyx px-6 text-base">
-            <div className="">
-              <UserCircleIcon className="h-8 w-8 cursor-pointer text-white_custom" />
+            <div className="cursor-pointer text-roman_silver transition hover:text-white_custom">
+              <UserCircleIcon className="h-8 w-8" />
             </div>
             <div className="text-white_custom">Guest Account</div>
-            <div className="ml-auto">
-              <EllipsisHorizontalIcon className="h-8 w-8 cursor-pointer text-roman_silver" />
+            <div className="ml-auto cursor-pointer text-roman_silver transition hover:text-white_custom">
+              <EllipsisHorizontalIcon className="h-8 w-8" />
             </div>
           </div>
         </div>
